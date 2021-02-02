48d2b52fccd6aef96f83161cdedd7cbe706c8471
Kirk Charles Niverba <kirkniverba23@gmail.com>
2021-02-02 18:45:32 +0800

    Call API

--- lib/features/weather_search/presentation/bloc/weather_search_bloc.dart
+++ lib/features/weather_search/presentation/bloc/weather_search_bloc.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
 import 'package:meta/meta.dart';
 import '../../models/built_values/built_weather_data.dart';
 
@@ -15,8 +17,22 @@ class WeatherSearchBloc extends Bloc<WeatherSearchEvent, WeatherSearchState> {
     WeatherSearchEvent event,
   ) async* {
     if (event is WeatherSearchTriggered) {
-      // TODO: Call to API
-      // Emit State
+      Dio dio = new Dio();
+      try {
+        Response response = await dio
+            .get("api.openweathermap.org/data/2.5/weather", queryParameters: {
+          "q": event.city,
+          "appid": "658e174916573a413b2f31b643a7b278"
+        });
+
+        BuiltWeatherData _weatherData =
+            BuiltWeatherData.fromJson(json.encode(response.data));
+
+        yield WeatherSearchLoaded(_weatherData);
+      } on DioError catch (e) {
+        yield WeatherSearchLoadFailure(
+            e.response.statusCode, e.response.statusMessage);
+      }
     }
   }
 }
